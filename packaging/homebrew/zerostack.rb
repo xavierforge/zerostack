f13441c502aff7f3e114af629f25ca4dd1497a43
Giuseppe <djtech.developer@gmail.com>
2026-06-14 16:17:24 +0200

    chore: bump version to 1.5.0

--- packaging/homebrew/zerostack.rb
+++ packaging/homebrew/zerostack.rb
@@ -1,25 +1,25 @@
 class Zerostack < Formula
   desc "Minimalistic coding agent written in Rust, optimized for memory footprint and performance"
   homepage "https://github.com/gi-dellav/zerostack"
-  version "1.4.5"
+  version "1.5.0"
   license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/gi-dellav/zerostack/releases/download/v1.4.5/zerostack-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/gi-dellav/zerostack/releases/download/v1.5.0/zerostack-x86_64-apple-darwin.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     else
-      url "https://github.com/gi-dellav/zerostack/releases/download/v1.4.5/zerostack-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/gi-dellav/zerostack/releases/download/v1.5.0/zerostack-aarch64-apple-darwin.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/gi-dellav/zerostack/releases/download/v1.4.5/zerostack-x86_64-unknown-linux-musl.tar.gz"
+      url "https://github.com/gi-dellav/zerostack/releases/download/v1.5.0/zerostack-x86_64-unknown-linux-musl.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     else
-      url "https://github.com/gi-dellav/zerostack/releases/download/v1.4.5/zerostack-aarch64-unknown-linux-musl.tar.gz"
+      url "https://github.com/gi-dellav/zerostack/releases/download/v1.5.0/zerostack-aarch64-unknown-linux-musl.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
